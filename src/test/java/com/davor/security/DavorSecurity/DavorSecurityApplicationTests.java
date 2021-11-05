461818ea54fae007158dbfc1ac0a9ab6c97eadf3
Jorge Lara <larajorge11@gmail.com>
2021-11-04 20:33:38 -0500

    feat(first commit, creation maven project)

---
+++ src/test/java/com/davor/security/DavorSecurity/DavorSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davor.security.DavorSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavorSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
