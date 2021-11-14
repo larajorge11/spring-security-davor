9369231c930bd0c0d80ec59130e838247b02c88f
Jorge Lara <larajorge11@gmail.com>
2021-11-14 06:33:14 -0500

    feat(Authentication using the Default Password Encoder)

--- src/main/java/com/davor/security/DavorSecurity/config/DavorSecurityConfig.java
+++ src/main/java/com/davor/security/DavorSecurity/config/DavorSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.davor.security.DavorSecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
@@ -21,5 +26,23 @@ public class DavorSecurityConfig extends WebSecurityConfigurerAdapter {
         });
         http.formLogin();
         http.httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        final UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+
+        final UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("jacobo")
+                .password("20210712")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
--- src/test/java/com/davor/security/DavorSecurity/web/controllers/BeerControllerTest.java
+++ src/test/java/com/davor/security/DavorSecurity/web/controllers/BeerControllerTest.java
@@ -3,11 +3,27 @@ package com.davor.security.DavorSecurity.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @WebMvcTest
 class BeerControllerTest extends BaseIT {
+
+    @Test
+    void authenticateUserSuccess() throws Exception {
+        mockMvc.perform(get("/api/v1/beer").with(httpBasic("user", "password")))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void authenticateUserForbidden() throws Exception {
+        mockMvc.perform(get("/api/v1/beer").with(httpBasic("jorge", "password")))
+                .andExpect(status().isUnauthorized());
+
+    }
 
     @Test
     void findBeers() throws Exception {
